b59437e085154cd80ad7c7efa5cb3e132d59620c
Jobayer Hossain <76811632+Jobayer-000@users.noreply.github.com>
2022-03-15 00:06:55 +0600

    Update cutenet_model.py

--- efficientnetv2/cutenet_model.py
+++ efficientnetv2/cutenet_model.py
@@ -66,7 +66,7 @@ def window_reverse(windows, window_size, H, W, C):
     x = tf.reshape(windows, shape=[-1, H // window_size,
                    W // window_size, window_size, window_size, C])
     x = tf.transpose(x, perm=[0, 1, 3, 2, 4, 5])
-    x = tf.reshape(x, shape=[B, H, W, C])
+    x = tf.reshape(x, shape=[-1, H, W, C])
     return x
 
 
